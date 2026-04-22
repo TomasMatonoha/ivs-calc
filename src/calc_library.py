f3dd0751498e94dc180fd13f1151521669fa29d8
Marie Patermannova <xpaterm00stud.fit.vut.cz>
2026-04-22 22:49:13 +0200

    Fix functions: factorial, sqr, power, comb_num

--- src/calc_library.py
+++ src/calc_library.py
@@ -1,4 +1,3 @@
-import math
 
 def add(a, b):
     return a+b # funkce vraci sectena cisla
@@ -16,19 +15,39 @@ def div(a, b):
     return a/b # funkce vydeli dve cisla
 
 def factorial(number):
-    return math.factorial(number) # funkce spocita faktorial
+    if not isinstance(number, int):
+        raise TypeError
+    elif (number < 0):
+        raise ValueError
+    else:
+        result = 1
+        while (number > 0):
+            result*= number
+            number -= 1
+    return result # funkce spocita faktorial
 
 def sqr(a, exp):
     if (a < 0) and (exp%2 == 0):
         raise AssertionError
     else:
-        return pow(a, 1/exp)
+        result = a ** (1/exp)
+        return result
 
 def power(a, exp):
     if isinstance(exp, int) and exp > 0:
-        return pow(a, exp) #umocni cislo exponentem
+        result = 0
+        reuslt = a ** exp
+        return result #umocni cislo exponentem
+    elif (exp == 0):
+        return 1
     else:
         raise AssertionError
 
 def comb_num(n , k):
-    return math.comb(n ,k) #vypocet kombinacniho cisla (n nad k)
+    if not isinstance(n,int) or not isinstance(k,int):
+        raise TypeError
+    elif (n < k) or (n < 0) or (k < 0):
+        raise ValueError
+    else:
+        result = factorial(n)//(factorial (n-k) * factorial(k))
+        return result #vypocet kombinacniho cisla (n nad k)
